5b807b2bd0ceb5cb1846a1675a0891fe46c616e3
Afaq Khan <101743351+afaq-khan2000@users.noreply.github.com>
2023-02-23 21:20:42 +0500

    Update Nft.js

--- models/Nft.js
+++ models/Nft.js
@@ -2,19 +2,50 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const nftSchema = new Schema({
+  tokenId: { type: String },
   name: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, default: Date.now(), required: true },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "users",
-    // required: true,
-  },
-  price: { type: Number, required: true },
   file: {
     type: String,
-     required: true
+    required: true,
   },
+  // smartContract: {
+  //   address: {
+  //     type: String,
+  //     required: true,
+  //   },
+  //   owner: {
+  //     type: String,
+  //     required: true,
+  //   },
+  //   royalties: {
+  //     type: Number,
+  //     required: true,
+  //   },
+  //   transferable: {
+  //     type: Boolean,
+  //     required: true,
+  //   },
+  // },
+  // blockchain: {
+  //   network: {
+  //     type: String,
+  //     required: true,
+  //   },
+  //   chainID: {
+  //     type: Number,
+  //     required: true,
+  //   },
+  //   blockHash: {
+  //     type: String,
+  //     required: true,
+  //   },
+  //   timestamp: {
+  //     type: Date,
+  //     required: true,
+  //   },
+  // },
 });
 
 module.exports = Nft = mongoose.model("nfts", nftSchema, "nfts");
